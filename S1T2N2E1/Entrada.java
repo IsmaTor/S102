e19142c8e952fb25aea49ae3fc10016abef92f94
Ismael <Ismael.Tortosa@hotmail.com>
2022-10-04 13:18:01 +0200

    corregido

--- S1T2N2E1/Entrada.java
+++ S1T2N2E1/Entrada.java
@@ -11,16 +11,16 @@ public class Entrada {
 	public static byte llegirByte(String missatge) throws InputMismatchException {
 		byte edat = 0;
         boolean continua; 
+        continua = false;
         do { //crea el bucle
             try {//es preveuen excepcions al programa
-                continua = false;
                 System.out.print("Introdueix la teva edat: ");
                 edat = entrada.nextByte();
             } catch (InputMismatchException ex) {
                 System.out.println("Error de format.");
-                entrada.next();
                 continua = true;
             }
+            entrada.nextLine(); //neteja de buffer
         } while (continua);//condición para fin del bucle
 		return edat;	
         }
@@ -28,16 +28,16 @@ public class Entrada {
 	public static int llegirInt(String missatge) throws InputMismatchException {
 		int edat = 0;
 		boolean continua;
+		continua = false;
 		do {
 			try {
-				continua = false;
 				System.out.println("Introdueix la teva edat: ");
 				edat = entrada.nextInt();
 			} catch (InputMismatchException ex) {
 				System.out.println("Error de format.");
-				entrada.next();
 				continua = true;
 			}
+			entrada.nextLine();
 		} while (continua);
 		return edat;
 	}
@@ -45,16 +45,16 @@ public class Entrada {
 	public static float llegirFloat(String missatge) throws InputMismatchException{
 		float edat = 0;
 		boolean continua;
+		continua = false;
 		do {
 			try {
-				continua = false;
 				System.out.println("Introdueix la teva edat; ");
 				edat = entrada.nextFloat();
 			} catch (InputMismatchException ex) {
 				System.out.println("Error de format.");
-				entrada.next();
 				continua = true;
 			}
+			entrada.nextLine();
 		} while (continua);
 		return edat;
 	}
@@ -62,16 +62,16 @@ public class Entrada {
 	public static double llegirDouble(String missatge) throws InputMismatchException {
 		double edat = 0;
 		boolean continua;
+		continua = false;
 		do {
 			try {
-				continua = false;
 				System.out.println("Introdueix la teva edat: ");
 				edat = entrada.nextDouble();
 			} catch (InputMismatchException ex) {
 				System.out.println("Error de format.");
-				entrada.next();
 				continua = true;
 			}
+			entrada.nextLine();
 		} while (continua);
 		return edat;
 	}
@@ -80,9 +80,9 @@ public class Entrada {
 	public static char llegirChar(String missatge) throws Exception {
 		
 		boolean continua;
+		continua = false;
 		do {
 			try {
-				continua = false;
 				System.out.println("Introdueix un caràcter: ");
 				missatge = entrada.next();
 						if (missatge.length() !=1) {
@@ -91,9 +91,9 @@ public class Entrada {
 						}
 			} catch (Exception ex) {
 				System.out.println("Error de format.");
-				entrada.next();
 				continua = true;
 			}
+			entrada.nextLine();
 		} while (continua);
 		
 		return missatge.charAt(0);
@@ -101,16 +101,16 @@ public class Entrada {
 	
 	public static String llegirString(String missatge) {
 		boolean continua;
+		continua = false;
 		do {
 			try {
-				continua = false;
 				System.out.println("Introdueix la teva paraula: ");
 				missatge = entrada.next();
 			} catch (Exception ex) {
 				System.out.println("Error de format. ");
-				entrada.next();
 				continua = true;
 			}
+			entrada.nextLine();
 		} while (continua);
 		return missatge;
 	}
@@ -118,31 +118,31 @@ public class Entrada {
 	public static boolean llegirSiNo(String missatge) throws Exception {
 		String afirmatiu = "S";
 		String negatiu = "N";
-		boolean continua = false;
+		boolean continua = true;
 		boolean resposta = false;
 		
 		do { 
 			try {
 				System.out.println("Introdueix Si (S) o No (N): ");
-				missatge = entrada.next();
-				continua = true;
-				if (missatge.equalsIgnoreCase(afirmatiu)) { //la letra ha de coincidir
+				missatge = entrada.next().toUpperCase(); // fer majúscules l'String
+				continua = false;
+				if (missatge.charAt(0) == 'S') { //agafarà la lletra serà la de la posició
 					continua = true;
 					resposta = true;
-				} else if (missatge.equalsIgnoreCase(negatiu)) { //la letra ha de coincidir
+				} else if (missatge.charAt(0) == 'N') { //agafarà la lletra serà la de la posició
 					continua = true;
 					resposta = false;
-				} else { // si no coinciden las letras al compararlas es false
+				} else { // si no coincideixen donarà false
 					System.out.println("Error de formato.");
 					continua = false; 
 					}
 				
 			} catch (Exception ex) {
 				System.out.println("Error de format.");
-				entrada.next();
 				continua = false;
 			}
-			} while (continua == false); // fin del bucle
+			entrada.nextLine();
+			} while (continua == false); // fi del bucle
 		
 		return resposta ;
 	}
